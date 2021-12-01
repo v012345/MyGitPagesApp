847f1b63c54fda81a03f3a16b4f4234307c014a9
v138468 <v138468@gmail.com>
2021-12-01 17:19:26 +0800

    o

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,35 +1,9 @@
 <template>
 	<view class="content">
-		
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-		<navigator url="../profile/index">
-			<button type="default">123123123</button>
-		</navigator>
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-		<navigator url="../profile/index">
-			<button type="default">123123123</button>
-		</navigator>
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-		<navigator url="../profile/index">
-			<button type="default">123123123</button>
-		</navigator>
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-		<navigator url="../profile/index">
-			<button type="default">123123123</button>
-		</navigator>
-		<Directory></Directory>
+
+		<uni-card title="Title Text" thumbnail="" extra="Extra Information" note="Tips">
+			The body of the content, you can customize the content and style
+		</uni-card> <!-- <Directory></Directory> -->
 	</view>
 
 
